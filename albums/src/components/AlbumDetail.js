ba9e06ef348051b6240af5a7a12c4912be99fd43
milan <lanetteam.milans@gmail.com>
2018-01-25 16:44:48 +0530

    CardSection

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
+  debugger;
   return (
     <Card>
-      <Text>{props.data.title}</Text>
+      <CardSection>
+        <Text>{props.data.title}</Text>
+      </CardSection>
+
+      <CardSection>
+        <Text>{props.data.url}</Text>
+      </CardSection>
+
+      <CardSection>
+        <Text>{props.data.image}</Text>
+      </CardSection>
     </Card>
   );
 };
+
 
 export default AlbumDetail;
--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { View } from 'react-native';
 
 const Card = (props) => {
-  debugger;
   return (
     <View style={styles.containerStyle}>
       {props.children}
